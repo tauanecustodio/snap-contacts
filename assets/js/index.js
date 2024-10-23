9be54425ff28e805274017d6c588c7da6fdb712a
Tauane Custodio <tauanesantoscustodio@gmail.com>
2024-10-23 11:06:41 -0300

    feat: add form validation and clear logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,6 +15,7 @@ const formAddContacts = document.getElementById('form-add-contacts');
 const inputName = document.getElementById('name-input');
 const inputTel = document.getElementById('tel-input');
 const inputEmail = document.getElementById('email-input');
+const errorMessage = document.getElementById('error-message');
 
 // table
 const tableBody = document.getElementById('contacts-table-body');
@@ -24,6 +25,25 @@ const totalContacts = document.getElementById('total-contacts');
 
 function modalToggle() {
   modalAddContacts.classList.toggle('hide');
+}
+
+function validateForm(name, tel, email) {
+  let message;
+  const telRegex = /^[0-9]{9,}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if(!name) {
+    message = "Preencha o nome do contato";
+  } else if(!tel) {
+    message = "Preencha o telefone do contato";
+  } else if (tel && !telRegex.test(tel)) {
+    message = "O telefone deve ter pelo menos 9 dígitos numéricos.";
+  } else if (email && !emailRegex.test(email)) {
+    message = "Formato de e-mail inválido.";
+  } else {
+    message = true;
+  }
+  return message
 }
 
 function addContactToTable(name, tel, email) {
@@ -53,13 +73,22 @@ function updateContactsList(name, tel, email) {
   };
 
   contactsList.push(contact);
-  console.log(contactsList);
 }
 
 function updateTotalContacts() {
   totalContacts.textContent = `${contactsList.length} contatos`;
 }
 
+function clearErrorMessage() {
+  errorMessage.textContent = '';
+}
+
+function clearForm() {
+  inputName.value = '';
+  inputTel.value = '';
+  inputEmail.value = '';
+  clearErrorMessage();
+}
 
 // --------- event listeners ---------
 
@@ -79,10 +108,24 @@ formAddContacts.addEventListener('submit',  function(e) {
   const name = inputName.value;
   const tel = inputTel.value;
   const email = inputEmail.value;
+
+  const validationResult = validateForm(name, tel, email);
+  
+  if(validationResult !== true) {
+    errorMessage.textContent = validationResult;
+    return;
+  }
   
   addContactToTable(name, tel, email);
   updateContactsList(name, tel, email);
   updateTotalContacts();
 
+  clearForm();
+
   modalToggle();
 })
+
+// clear error message when typing in input
+inputName.addEventListener('input', clearErrorMessage)
+inputTel.addEventListener('input', clearErrorMessage);
+inputEmail.addEventListener('input', clearErrorMessage);
