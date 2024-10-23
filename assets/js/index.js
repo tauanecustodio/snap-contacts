23e1a1081bef0ab03bdaa6dbbec22d394fc9d8ee
Tauane Custodio <tauanesantoscustodio@gmail.com>
2024-10-23 08:18:15 -0300

    feat: add logic to add contacts to table

---
+++ assets/js/index.js
@@ -0,0 +1,67 @@
+// --------- variables declaration ---------
+
+// main buttons
+const addContactBtn = document.getElementById('add-btn');
+
+// modal
+const modalAddContacts = document.getElementById('modal__add-contacts');
+const closeModalBtn = document.querySelector('.modal__cancel');
+const addContactModalBtn = document.querySelector('.modal__add');
+
+// form
+const formAddContacts = document.getElementById('form-add-contacts');
+const inputName = document.getElementById('name-input');
+const inputTel = document.getElementById('tel-input');
+const inputEmail = document.getElementById('email-input');
+
+// table
+const tableBody = document.getElementById('contacts-table-body');
+
+// --------- function declarations ---------
+
+function modalToggle() {
+  modalAddContacts.classList.toggle('hide');
+}
+
+function addContactToTable(name, tel, email) {
+  const row = document.createElement('tr');
+  
+  const nameCell = document.createElement('td');
+  nameCell.textContent = name;
+  
+  const telCell = document.createElement('td');
+  telCell.textContent = tel;
+  
+  const emailCell = document.createElement('td');
+  emailCell.textContent = email;
+  
+  row.appendChild(nameCell);
+  row.appendChild(telCell);
+  row.appendChild(emailCell);
+  
+  tableBody.appendChild(row);
+}
+
+
+// --------- event listeners ---------
+
+addContactBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalToggle();
+})
+
+closeModalBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalToggle();
+})
+
+formAddContacts.addEventListener('submit',  function(e) {
+  e.preventDefault();
+  
+  const name = inputName.value;
+  const tel = inputTel.value;
+  const email = inputEmail.value;
+  
+  addContactToTable(name, tel, email);
+  modalToggle();
+})
